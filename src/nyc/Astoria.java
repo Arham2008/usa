c5ac5d137b8b06e9788c04fdb68e0c878ebfae6e
arham2008 <akbarmunner@gmail.com>
2020-09-09 22:05:56 -0400

    usa project updated

--- src/nyc/Astoria.java
+++ src/nyc/Astoria.java
@@ -2,8 +2,24 @@ package nyc;
 
 public class Astoria {
 
+	
+	int myNum=123;
+	static int  price =230;
+	
 	public static void main(String[] args) {
 		System.out.println("how are you");
+		
+		
+		Astoria art =new Astoria();
+		System.out.println(art.myNum);
+		 art.myNum=345;
+		
+		System.out.println(art.myNum);
+		
+       System.out.println(Astoria.price);
+       Astoria.price=400;
+       System.out.println(Astoria.price);
+       
 	}
 
 }
